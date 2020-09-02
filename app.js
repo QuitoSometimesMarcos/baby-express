9638770e3d8327ab5d1ca04644243c5454ce5759
feychou <federica.recanatini@gmail.com>
2020-09-02 11:45:14 +0200

    Add file upload

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const multer  = require('multer');
 
 const app = express();
+const publicFolder = path.join(__dirname, 'public');
+const uploadFolder = path.join(publicFolder, 'uploads');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadFolder)
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+const upload = multer({ storage })
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(publicFolder));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -14,8 +28,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-  res.sendFile('homepage.html', {root: path.join(__dirname, 'public')})
+  res.sendFile('homepage.html', {root: publicFolder })
 });
+
+app.get('/upload-profile-picture', (req, res) => {
+  res.sendFile('upload_profile_picture.html', {root: publicFolder })
+});
+
+app.post('/upload-profile-picture', upload.single('profile_pic'), (req, res) => {
+  const { file, fileValidationError } = req
+  if (!file) {
+    return res.status(400).send('Please upload a file');
+  }
+  if (fileValidationError) {
+    return res.status(400).send(fileValidationError);
+  }
+  console.log(file)
+  res.send(`<div>You have uploaded this image: <br/> <img src="http://localhost:3000/uploads/${req.file.filename}" width="500" /></div>`);
+})
+
 
 app.put('/home', (req, res) => {
   res.json({'good' : 'yep'})
